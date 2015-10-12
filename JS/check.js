5e383478cf7d8bf44cd0f45a173bafdfcb7ad929
cris <cristeam2@cristeam.local>
2015-10-12 19:47:28 +0200

    commit V2

--- JS/check.js
+++ JS/check.js
@@ -5,33 +5,14 @@
 }*/
 
 
-function compruebaPassword(){ 
-    if (document.f1.password.value == "" || document.f1.password.value.length <2 ){ 
-        //si era la cadena vacía es que no era válido. Lo aviso 
-        alert ("La password debe contener almenos 2 caracteres!")         
-        document.f1.password.select() 
-        //coloco otra vez el foco 
-        document.f1.password.focus() 
-    }
-}
-
-function compruebaPasswordNueva(){ 
-    if (document.f1.passwordNueva.value == "" || document.f1.passwordNueva.value.length <2 ){ 
-        //si era la cadena vacía es que no era válido. Lo aviso 
-        alert ("La password debe contener almenos 2 caracteres!")         
-        document.f1.passwordNueva.select() 
-        //coloco otra vez el foco 
-        document.f1.passwordNueva.focus() 
-    }
-}
 
 function compruebaUsuario(){ 
-    if (document.f1.usuario.value.length <2 ){ 
+    if (document.getElementById("nombre").value.length <2){
         //si era la cadena vacía es que no era válido. Lo aviso 
-        alert ("El usuario debe ser de almenos 2 caracteres") 
+        alert("El usuario debe ser de almenos 2 caracteres") ;
         //selecciono el texto 
-        document.f1.usuario.select() 
+        document.getElementById("nombre").select();
         //coloco otra vez el foco 
-        document.f1.usuario.focus() 
+        document.getElementById("nombre").focus();
     }
 }
